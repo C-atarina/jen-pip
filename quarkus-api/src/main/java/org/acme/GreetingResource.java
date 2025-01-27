4c211217ca1556ac88392962963e5c1443762d57
Catarina Roriz <catarinaroriz08@gmail.com>
2025-01-27 10:10:15 +0100

    Updt

--- quarkus-api/src/main/java/org/acme/GreetingResource.java
+++ quarkus-api/src/main/java/org/acme/GreetingResource.java
@@ -9,8 +9,8 @@ import jakarta.ws.rs.core.MediaType;
 public class GreetingResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
     public String hello() {
-        return "Hello from Quarkus REST with update";
+        return "Hello from Quarkus REST with json response";
     }
 }
